b188dc7b8671a85bb649200a3d9dfe27263aa22c
gun <gungungggun@gmail.com>
2017-08-19 11:22:38 +0900

    classで書き換え

--- src/index.js
+++ src/index.js
@@ -1,36 +1,42 @@
-var allA = document.getElementsByTagName("a");
-var aList = Array.prototype.slice.call(allA);
+class TBlank {
+  constructor() {
+    this.allA = document.getElementsByTagName("a")
+    this.aList = Array.prototype.slice.call(this.allA)
+    this.device = this.getDevice();
 
-var device = getDevice();
-
-if (device == 'other') {
-  aList.forEach(function (item) {
-    var url = item.getAttribute("href");
-    if (!isInternalLink(url)) {
-      item.setAttribute("target", "_blank");
+    if (this.device == 'other') {
+      self = this
+      this.aList.forEach(function (item) {
+        let url = item.getAttribute("href")
+        if (!self.isInternalLink(url)) {
+          item.setAttribute("target", "_blank")
+        }
+      })
     }
-  });
-}
+  }
 
-function isInternalLink(url) {
-  if (typeof(url) != 'string') {
-    return false;
-  } else if (url.indexOf(location.protocol + '//' + location.host) == 0) {
-    return true;
-  } else if (url.match(/^https?:\/\//)) {
-    return false;
-  } else {
-    return true;
+  isInternalLink(url) {
+    if (typeof(url) != 'string') {
+      return false
+    } else if (url.indexOf(location.protocol + '//' + location.host) == 0) {
+      return true
+    } else if (url.match(/^https?:\/\//)) {
+      return false
+    } else {
+      return true
+    }
+  }
+
+  getDevice() {
+    var ua = window.navigator.userAgent
+    if(ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0){
+        return 'sp'
+    }else if(ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0){
+        return 'tab'
+    }else{
+        return 'other'
+    }
   }
 }
 
-function getDevice() {
-  var ua = window.navigator.userAgent;
-  if(ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0){
-      return 'sp';
-  }else if(ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0){
-      return 'tab';
-  }else{
-      return 'other';
-  }
-}
+new TBlank()
