51d57bde4120374a334fc929040ff2c4c714b87a
gun <gungungggun@gmail.com>
2017-11-14 11:10:18 +0900

    記述調整

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 class TBlank {
   constructor() {
-    self = this
+    let self = this
     document.addEventListener('DOMContentLoaded', function() {
-      self.allA = document.getElementsByTagName("a")
+      self.allA = document.getElementsByTagName('a')
       self.aList = Array.prototype.slice.call(self.allA)
       self.device = self.getDevice();
 
       if (self.device == 'other') {
         self.aList.forEach(function (item) {
-          let url = item.getAttribute("href")
+          let url = item.getAttribute('href')
           if (!self.isInternalLink(url)) {
-            item.setAttribute("target", "_blank")
+            item.setAttribute('target', '_blank')
           }
         })
       }
@@ -30,7 +30,7 @@ class TBlank {
   }
 
   getDevice() {
-    var ua = window.navigator.userAgent
+    const ua = window.navigator.userAgent
     if(ua.indexOf('iPhone') > 0 || ua.indexOf('iPod') > 0 || ua.indexOf('Android') > 0 && ua.indexOf('Mobile') > 0){
       return 'sp'
     }else if(ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0){
@@ -43,14 +43,14 @@ class TBlank {
   replace(val) {
     if (this.device == 'other') {
       let a = val.match(/<a(.|\s)*?>/gi)
-      self = this
+      let self = this
       if (a !== null) {
         a.forEach(v => {
           let x = v.match(/href="([^\"]+)"/)
           if (x !== null) {
             let url = x[1]
             if (!self.isInternalLink(url)) {
-              val = val.replace(x[1], x[1]+'" target="_blank')
+              val = val.replace(x[1], x[1] + '" target="_blank')
             }
           }
         })
@@ -60,6 +60,6 @@ class TBlank {
   }
 }
 
-var tblank = new TBlank
+const tblank = new TBlank
 module.exports = tblank
 module.exports.default = tblank
